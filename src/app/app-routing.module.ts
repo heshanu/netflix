5af17f65ce4aabd3fee880bd48d1dac191d98c2b
heshanu <heshanu97@gmail.com>
2023-12-27 21:21:02 +0530

    update dev structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './comp/login/login.component';
+import { NotfoundComponent } from './pages/notfound/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {path:'**',component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,43 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AntdesignModule } from './shared/antdesign/antdesign.module';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { MaterialdesignModule } from './shared/materialdesign/materialdesign.module';
+import { NavigationComponent } from './shared/navbar/navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { LoginComponent } from './comp/login/login.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound/notfound.component';
+import { SearchComponent } from './shared/navbar/search/search.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    HomeComponent,
+    NotfoundComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AntdesignModule,
+    NzBreadCrumbModule,
+    MaterialdesignModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     provideClientHydration()
